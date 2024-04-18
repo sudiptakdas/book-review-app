54e7fa8974223c48f816c4f3e4ab4b0460d578c0
Sudipta Kumar Das <sudiptadas2000k@gmail.com>
2024-04-18 22:11:44 +0530

    main:added frontend repo

---
+++ frontend/src/pages/Books.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Books: React.FC = () => {
+  return (
+    <div>
+      <h1 className=' text-4xl text-red-500'>Hola </h1>
+    </div>
+  );
+};
+
+export default Books;
